a3fe48f3698e685906c8cf95aaa9c5cc57e3b3fe
rsprojectexamples <149135780+rsprojectexamples@users.noreply.github.com>
2023-10-27 15:34:55 +0800

    Made number representation private and updated comment

--- operationNumberRepresentation.h
+++ operationNumberRepresentation.h
@@ -7,7 +7,7 @@
 
 struct OperationNumberRepresentation
 {
-    // Each number is represented
+    // Each number is represented as a series of operations and operands
     using OperationNumber = std::vector<std::pair<int (*)(int, int), std::optional<int>>>;
 
     // Monkey item operations
@@ -16,12 +16,13 @@ struct OperationNumberRepresentation
     static int divide(int a, int b) { return (int)std::floor(a / b); }
 
 public:
-    OperationNumber number;
     OperationNumberRepresentation(int value);
     OperationNumberRepresentation &operator+=(std::optional<int> value);
     OperationNumberRepresentation &operator*=(std::optional<int> value);
     OperationNumberRepresentation &operator/=(std::optional<int> value);
     int numberModulateBy(int modulator = std::numeric_limits<int>::max()) const;
+private: 
+    OperationNumber number;
 };
 
 #endif
